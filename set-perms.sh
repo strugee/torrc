8a8476320bf2099c0af62924396d794a0d762852
AJ Jordan <alex@strugee.net>
2017-08-03 07:04:47 +0000

    Add missing permissions

--- set-perms.sh
+++ set-perms.sh
@@ -2,5 +2,7 @@
 
 chmod 644 usr/local/etc/torrc
 chown root:wheel usr/local/etc/torrc
+chmod 555 usr/local/etc/rc.d/tor
+chown root:wheel usr/local/etc/rc.d/tor
 chmod 644 etc/rc.conf
 chown root:wheel etc/rc.conf
