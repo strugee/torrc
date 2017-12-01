4da29187d7f52295c304bd3565c6fe3d20ed4be9
AJ Jordan <alex@strugee.net>
2017-12-01 00:46:50 +0000

    Add pristine /etc/crontab

--- set-perms.sh
+++ set-perms.sh
@@ -6,6 +6,8 @@ chmod 644 usr/local/etc/torrc
 chown root:wheel usr/local/etc/torrc
 chmod 555 usr/local/etc/rc.d/tor
 chown root:wheel usr/local/etc/rc.d/tor
+chmod 644 etc/crontab
+chown root:wheel etc/crontab
 chmod 644 etc/rc.conf
 chown root:wheel etc/rc.conf
 chmod 644 etc/ntp.conf
