1fd6834879a0be096f7d7b496ac72b48c9202be3
AJ Jordan <alex@strugee.net>
2017-12-30 08:40:23 +0000

    Fix torrc location
    This is where it actually is in production.

--- set-perms.sh
+++ set-perms.sh
@@ -2,8 +2,8 @@
 
 chmod 755 usr/local/bin/find-interesting-logs
 chown root:wheel usr/local/bin/find-interesting-logs
-chmod 644 usr/local/etc/torrc
-chown root:wheel usr/local/etc/torrc
+chmod 644 usr/local/etc/tor/torrc
+chown root:wheel usr/local/etc/tor/torrc
 chmod 555 usr/local/etc/rc.d/tor
 chown root:wheel usr/local/etc/rc.d/tor
 chmod 644 etc/crontab
